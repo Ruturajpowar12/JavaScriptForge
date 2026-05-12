a733a69f356c4f114785288360520e22f998c75f
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-05-12 16:00:04 +0530

    Add array and practice examples, along with classes and objects implementation

---
+++ JS-Concepts/Classes-Objects/classes.js
@@ -0,0 +1,13 @@
+//classes & objects in Js
+
+//objects    // prototype
+
+let student = {
+    name: "Ruturaj",
+    age: 23,
+    printage: function () {
+        console.log("age :", this.age);
+    },
+};
+
+
