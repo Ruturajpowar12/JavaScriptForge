dd76b60b95c4e7290a716bb983acc266222196fd
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-05-12 21:43:10 +0530

    Add class and inheritance examples, including error handling with try-catch

--- JS-Concepts/Classes-Objects/classes.js
+++ JS-Concepts/Classes-Objects/classes.js
@@ -11,3 +11,63 @@ let student = {
 };
 
 
+//classes
+
+class MyClass {
+    constructor() {
+
+    }
+
+    newMethod() {
+        console.log("this class");
+    }
+}
+
+let obj = new MyClass();
+obj.newMethod();
+
+
+//inheritance
+
+class parent {
+
+}
+
+class child extends parent {
+
+}
+
+//super keyword
+
+super();
+
+
+//qu1
+
+class college {
+
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    viewData() {
+        console.log("Name is :", name);
+        console.log("Email is :", email);
+    }
+}
+
+let obj = new college("Ruturaj", "Rutu@gmail.com");
+
+obj.viewData();
+
+// TRY CATCH
+
+let a = 5;
+let b = 10;
+try {
+    console.log("a+b", a + c);
+} catch (err) {
+
+    console.log(err);
+}
