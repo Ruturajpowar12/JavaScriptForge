d4b913856f7412b752b3217aa4de63bd5152e153
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-05-15 12:53:58 +0530

    Heart Trail project with interactive mouse effects

---
+++ Heart-Trail/script.js
@@ -0,0 +1,23 @@
+
+const body = document.querySelector("body");
+
+body.addEventListener("mousemove", (event) => {
+    const x = event.offsetX
+    const y = event.offsetY
+
+    const span = document.createElement("span");
+
+    span.style.left = x + "px";
+    span.style.top = y + "px";
+
+    const size = Math.random() * 100;
+
+    span.style.width = size + "px";
+    span.style.height = size + "px";
+
+
+    body.appendChild(span)
+    setTimeout(() => {
+        span.remove();
+    }, 3000);
+})
