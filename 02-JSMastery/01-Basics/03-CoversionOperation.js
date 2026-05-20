9c92567fc3d96e1578c0cb0ef424750b46c9eb40
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-05-20 15:34:43 +0530

    Add Conversion  examples for data types

---
+++ 02-JSMastery/01-Basics/03-CoversionOperation.js
@@ -0,0 +1,34 @@
+//conversions of data Types
+
+
+let score = "rutu"
+
+console.log(typeof(score));
+
+let valueInNum = Number(score)
+
+console.log(typeof valueInNum);
+console.log(valueInNum)
+
+//"33" - 33
+// "33abc" - NaN (Not a Number)
+//true - 1; false - 0;
+
+let isLogged = "Rp";
+
+let  booleanIsLog = Boolean(isLogged)
+console.log(typeof booleanIsLog);
+
+console.log(booleanIsLog);
+
+//1-true  ; 0 -false
+//" " - false
+//" Rp" - true
+
+
+let someNum = 33
+
+let stringNum = String(someNum)
+console.log(typeof stringNum);
+
+console.log(stringNum);
