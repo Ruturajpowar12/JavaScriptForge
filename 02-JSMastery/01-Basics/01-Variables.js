80cff1eb4762fadd7aaf9732768018c25c2469ee
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-05-20 15:18:37 +0530

    Add variable and data type examples in 01-basic

---
+++ 02-JSMastery/01-Basics/01-Variables.js
@@ -0,0 +1,21 @@
+//Variables and constants
+
+//const - can not be re-declared & can be update
+// let - can not be re-declared & can be update //
+
+// Var - can be declared & can be upadate
+//  perfer not to use var  because of issue in block scope & functional scope 
+
+
+const accId = 1273
+let accEmail = "ram@gmail.com"
+var accPass  = "1233"
+
+accCity = "pune"
+
+let name ; // undefined
+
+console.log(accId);
+
+
+console.table([accId,accEmail,accPass,accCity,name])
---
+++ 02-JSMastery/01-Basics/02-DataTypes.js
@@ -0,0 +1,26 @@
+"use strict "; // treat all Js code as newer  version
+
+//alert("hello") we are using nodejs , not browser
+
+console.log(3+3)
+console.log("hello")
+
+//data type in JS
+
+let name = "Ruturaj"     //string 
+let age  = 18           //Number
+let isLoggedIn = false   //Boolean
+let state = null         //Null
+
+// number - 2 to power 53
+//bigint 
+//string - "" &  ''
+//boolean - true/false
+//null - standalone value  null is object
+//undefined -  value not assigned
+//symbol  - unique
+
+
+//object 
+
+console.log(typeof());
