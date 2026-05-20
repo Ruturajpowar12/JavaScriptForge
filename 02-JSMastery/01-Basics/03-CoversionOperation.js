ce5d6716764e7b9c181595f58a9cd22a3d96a1d1
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-05-20 15:57:26 +0530

    Conversion Operations with Datatypes

--- 02-JSMastery/01-Basics/03-CoversionOperation.js
+++ 02-JSMastery/01-Basics/03-CoversionOperation.js
@@ -32,3 +32,47 @@ let stringNum = String(someNum)
 console.log(typeof stringNum);
 
 console.log(stringNum);
+
+//****************  Operations *****************
+
+
+let  value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 / 2)
+// console.log(2 ** 2)
+// console.log(2 % 2)
+
+let str1 = "hello"
+let str2 ="Ruturaj"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+ 2);
+console.log( 1 + "2");
+console.log("1"+ 2 +2);
+console.log( 1 + 2 + "2");
+
+console.log(+true); // 1
+console.log(+"")   //0
+
+let gameCounter =100
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
