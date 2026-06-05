48a48bb31dcf86ea0580dfadfa24d7edda7ef580
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-06-05 14:57:08 +0530

    update reset button in counter app

--- Js-Practice/practice-project/Counter App/app.js
+++ Js-Practice/practice-project/Counter App/app.js
@@ -1,17 +1,33 @@
 const h1 = document.querySelector("h1")
 const increase = document.querySelector(".increase")
 const decrease = document.querySelector(".decrease")
+const reset = document.querySelector("#reset")
 
-increase.addEventListener("click",(e)=>{
-    h1.innerText++
-   console.log(h1.innerText);
-   
-    
-})
-
-decrease.addEventListener("click",()=>{
-    if(h1.innerText != 0){
-     h1.innerText--
+function updateButtonState() {
+    if (h1.innerText === "0") {
+        decrease.style.backgroundColor = "grey"
+        decrease.style.pointerEvents = "none" 
+    } else {
+        decrease.style.backgroundColor = "" 
+        decrease.style.pointerEvents = "auto"
     }
-     console.log(h1.innerText);
+}
+
+increase.addEventListener("click", () => {
+    h1.innerText++
+    updateButtonState()
 })
+
+decrease.addEventListener("click", () => {
+    if (h1.innerText != 0) {
+        h1.innerText--
+    }
+    updateButtonState()
+})
+
+reset.addEventListener("click", () => {
+    h1.innerText = 0
+    updateButtonState()
+})
+
+updateButtonState()
