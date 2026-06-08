2c10f8241d0da8608ab84e845f3a8fae6628d59f
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-06-08 19:29:40 +0530

    add password copy functionality with animated popup notification

--- Password-genrator/app.js
+++ Password-genrator/app.js
@@ -1,19 +1,20 @@
 const passBox = document.querySelector("input")
 const copyBtn = document.querySelector("#copy")
 const btn = document.querySelector("#gen-pass")
-const popup = document.querySelector(".box .c")
 
 const length = 12
 
 const upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 const lowerCase = "abcdefghijklmnopqrstuvxyz"
 const number ="0123456789"
 const Symbol = "@#$%^&*()_+|<>{}[]/?=-"
 
-const allChars = upperCase + lowerCase + number + Symbol
 
+const allChars = upperCase + lowerCase + number + Symbol
 function craetePassword(){
-    let password = ""
+
+    let password =" "
     password += upperCase[Math.floor(Math.random() * upperCase.length)]
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
     password += number[Math.floor(Math.random() * number.length)]
@@ -30,13 +31,7 @@ btn.addEventListener("click",()=>{
 })
 
 copyBtn.addEventListener("click",()=>{
-    if(passBox.value === "") return;
-    
     passBox.select()
     document.execCommand("copy")
     
-    popup.classList.add("show")
-    setTimeout(()=>{
-        popup.classList.remove("show")
-    }, 1500)
 })
