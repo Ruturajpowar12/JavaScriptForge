20d4df572c943edac30dcee04bbaf57bd974ddda
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-06-09 18:06:06 +0530

    add Control Flow (Part 3) with Day Finder, Month Finder, Calculator, and Mini Project tasks

--- Concept-Learning/02-ControlFlow.js
+++ Concept-Learning/02-ControlFlow.js
@@ -110,3 +110,134 @@ if (number === secretNumber) {
 } else {
   console.log("Too Low");
 }
+
+//Module 2: Control Flow (Part 3)
+
+//Practical Task 1: Day Finder
+
+let day = Number(prompt("Enter a Choice of day :"));
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wedsnday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Invalid Day");
+}
+
+//Practical Task 2: Month Finder
+
+let month = Number(prompt("Enter a month number : "));
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("May");
+    break;
+  case 6:
+    console.log("June");
+    break;
+  case 7:
+    console.log("July");
+    break;
+  case 8:
+    console.log("August");
+    break;
+  case 9:
+    console.log("September");
+    break;
+  case 10:
+    console.log("October");
+    break;
+  case 11:
+    console.log("November");
+    break;
+  case 12:
+    console.log("December");
+    break;
+
+  default:
+    console.log("Invalid Month");
+}
+
+// Practical Task 3: Calculator using switch
+
+let num1 = Number(prompt("Enter first number"));
+let operator = prompt("Enter operator (+,-,*,/):");
+let num2 = Number(prompt("Enter second number"));
+
+switch (operator) {
+  case "+":
+    console.log(num1 + num2);
+    break;
+  case "-":
+    console.log(num1 - num2);
+    break;
+  case "*":
+    console.log(num1 * num2);
+    break;
+  case "/":
+    console.log(num1 / num2);
+    break;
+  default:
+    console.log("Invalid Operator");
+}
+
+//Mini Project
+
+console.log("1 → Pizza \n2 → Burger\n3 → Sandwich\n4 → Pasta");
+
+let Pizza = 250;
+let Burger = 120;
+let Sandwich = 100;
+let Pasta = 180;
+let choice = Number(prompt("Enter your choice :"));
+switch (choice) {
+  case 1:
+    console.log("You ordered Pizza");
+    console.log(`Price : ₹${Pizza}`);
+    break;
+  case 2:
+    console.log("You ordered Burger");
+    console.log(`Price : ₹${Burger}`);
+    break;
+  case 3:
+    console.log("You ordered Sandwich");
+    console.log(`Price : ₹${Sandwich}`);
+    break;
+  case 4:
+    console.log("You ordered Pasta");
+    console.log(`Price : ₹${Pasta}`);
+    break;
+  default:
+    console.log("Invalid Choice");
+}
