68da6902a471c9f76876a0d98ad053ccf1702a6e
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-06-09 17:41:36 +0530

    add Control Flow (Part 2) with Grade Calculator, Age Category, Login System, and Number Guess Checker tasks

--- Concept-Learning/02-ControlFlow.js
+++ Concept-Learning/02-ControlFlow.js
@@ -51,3 +51,62 @@ if (marks >= 35) {
 } else {
   console.log("Fail");
 }
+
+//Module 2: Control Flow (Part 2)
+
+// Practical Task 1: Grade Calculator
+let marks = Number(prompt("Enter your marks"));
+if (marks >= 90 && marks <= 100) {
+  console.log("A+");
+} else if (marks >= 75 && marks <= 89) {
+  console.log("A");
+} else if (marks >= 60 && marks <= 74) {
+  console.log("B");
+} else if (marks >= 35 && marks <= 59) {
+  console.log("C");
+} else {
+  console.log("Fail");
+}
+
+//Practical Task 2: Age Category
+let age = Number(prompt("Enter your age :"));
+
+if (age >= 0 && age <= 12) {
+  console.log("Child");
+} else if (age >= 13 && age <= 19) {
+  console.log("Teenager");
+} else if (age >= 20 && age <= 59) {
+  console.log("Adult");
+} else {
+  console.log("Senior Citizen");
+}
+
+//Practical Task 3: Simple Login System
+
+const username = "admin";
+const password = "1234";
+
+let enteredUsername = prompt("Enter username");
+let enteredPassword = prompt("Enter password");
+
+if (
+  username === enteredUsername.trim() &&
+  password === enteredPassword.trim()
+) {
+  console.log("Login Successful");
+} else {
+  console.log("Invalid Username or Password");
+}
+
+//Number Guess Checker
+
+const secretNumber = 7;
+let number = Number(prompt("Enter a Number :"));
+
+if (number === secretNumber) {
+  console.log("Correct Guess");
+} else if (number > secretNumber) {
+  console.log("Too High");
+} else {
+  console.log("Too Low");
+}
