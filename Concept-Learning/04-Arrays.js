3a2f4497aaf682578a7f009234ff0bed306c0185
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-06-10 20:40:01 +0530

    add Arrays (Part 1) with practical tasks and mini projects

---
+++ Concept-Learning/04-Arrays.js
@@ -0,0 +1,34 @@
+// 🚀 Module 4: Arrays (Part 1)
+// Arrays store multiple values in one variable.
+
+// Practical Task 1
+const colors = ["Red", "Green", "Blue"];
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+
+// Practical Task 2
+const numbers = [10, 20, 30, 40, 50];
+console.log(`Length : ${numbers.length}`);
+
+// Practical Task 3
+const cities = ["Pune", "Mumbai", "Kolhapur"];
+cities[1] = "Nagpur";
+console.log(cities);
+
+// Practical Task 4
+const students = ["Ram", "Shyam", "Ruturaj"];
+console.log(students[0]);
+console.log(students[2]); //or
+console.log(students[students.length - 1]);
+
+//Mini Project ⭐
+const subjects = ["JavaScript", "React", "Node.js", "MongoDB"];
+console.log(`Total Subjects: ${subjects.length}`);
+
+//Mini Project ⭐⭐
+const marks = [80, 75, 90, 85, 70];
+
+console.log(marks[0]);
+console.log(marks[marks.length - 1]); //marks[4]
+console.log(marks.length);
