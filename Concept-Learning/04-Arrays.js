280869040efd434d8e6253493b70419dddd491a0
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-06-10 20:59:51 +0530

    add Arrays (Part 2) with practical tasks and mini projects focusing on array methods

--- Concept-Learning/04-Arrays.js
+++ Concept-Learning/04-Arrays.js
@@ -32,3 +32,47 @@ const marks = [80, 75, 90, 85, 70];
 console.log(marks[0]);
 console.log(marks[marks.length - 1]); //marks[4]
 console.log(marks.length);
+
+//Module 4: Arrays (Part 2)
+//Array Methods
+
+// Practical Task 1
+const colors = ["Red", "Green"];
+colors.push("blue");
+console.log(colors);
+
+// Practical Task 2
+const fruit = ["Apple", "Mango", "Banana"];
+fruit.pop();
+console.log(fruit);
+
+// Practical Task 3
+const languages = ["Javascript", "React"];
+languages.unshift("Html");
+console.log(languages);
+
+//Practical Task 4
+const city = ["Pune", "Mumbai", "Kolhapur"];
+city.shift();
+console.log(city);
+
+// Mini Project ⭐
+const technologies = ["Javascript", "React", "Node.js", "MongoDB"];
+technologies.push("Redux");
+// console.log(technologies);
+technologies.unshift("Css");
+// console.log(technologies);
+technologies.pop();
+console.log(technologies);
+
+// Mini Project ⭐⭐
+const cart = ["Milk", "Bread", "Eggs"];
+cart.push("Butter");
+// console.log(cart);
+cart.unshift("Cheese");
+// console.log(cart);
+cart.pop();
+// console.log(cart);
+cart.shift();
+console.log(cart);
+console.log(cart.length);
