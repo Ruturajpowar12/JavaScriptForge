7b81458fce525648988044d387894becdf8b4dd2
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-06-10 19:23:06 +0530

    add Functions (Part 2) with practical tasks and mini projects

--- Concept-Learning/03-Functions.js
+++ Concept-Learning/03-Functions.js
@@ -34,3 +34,61 @@ function table(number) {
   }
 }
 table(5);
+
+// Module 3: Functions (Part 2)
+
+// Practical Task 1
+function square(num) {
+  return num * num;
+}
+console.log(square(6));
+
+// Practical Task 2
+function cube(num) {
+  return num ** 3;
+}
+console.log(cube(3));
+
+// Practical Task 3
+function getFullName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
+console.log(getFullName("Ruturaj", "Powar"));
+
+// Practical Task 4
+function isPositive(num) {
+  if (num > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isPositive(11));
+// OR
+function isPositive(num) {
+  return num > 0;
+}
+
+console.log(isPositive(-1));
+
+// Mini Project ⭐
+function calculateArea(length, width) {
+  return length * width;
+}
+console.log(calculateArea(10, 5));
+
+// Mini Project ⭐⭐
+function calculateGrade(marks) {
+  if (marks >= 90 && marks <= 100) {
+    return "A+";
+  } else if (marks >= 75 && marks <= 89) {
+    return "A";
+  } else if (marks >= 60 && marks <= 74) {
+    return "B";
+  } else if (marks >= 35 && marks <= 59) {
+    return "C";
+  } else {
+    return "Fail";
+  }
+}
+console.log(calculateGrade(82));
