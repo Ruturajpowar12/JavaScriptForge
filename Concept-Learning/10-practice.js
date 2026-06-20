97020ef49b67e52350622e57e231b7473ee83016
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-06-20 14:59:28 +0530

    add: implement user management tasks with mapping, filtering, finding, and reducing functionalities

--- Concept-Learning/10-practice.js
+++ Concept-Learning/10-practice.js
@@ -1,3 +1,5 @@
+const { use } = require("react");
+
 // Task 1
 const employees = [
   { id: 1, name: "A", salary: 50000 },
@@ -29,3 +31,49 @@ const countingOccurrence = techs.reduce((acc, tech) => {
   return acc;
 }, {});
 console.log(countingOccurrence);
+
+// task1
+const users = [
+  { id: 1, name: "John", age: 28, active: true },
+  { id: 2, name: "Alex", age: 35, active: false },
+  { id: 3, name: "David", age: 22, active: true },
+  { id: 4, name: "Emma", age: 30, active: true },
+];
+const userNames = users.map((user) => user.name);
+console.log(userNames);
+
+//task2
+const activeUsers = users.filter((user) => user.active === true);
+console.log(activeUsers);
+
+//task3
+const userId = users.find((user) => user.name === "Emma");
+console.log(userId);
+
+//task4
+const averageAge = users.reduce((acc, curr) => {
+  const Total = acc + curr.age;
+  return Math.floor(Total / users.length);
+}, 0);
+console.log(averageAge);
+
+// /task5
+const higherAgeActiveUsers = users
+  .filter((user) => user.active === true && user.age > 25)
+  .map((user) => user);
+console.log(higherAgeActiveUsers);
+
+//task6
+const data = [
+  { department: "IT" },
+  { department: "HR" },
+  { department: "IT" },
+  { department: "Finance" },
+  { department: "HR" },
+];
+
+const countDepartments = data.reduce((acc, field) => {
+  acc[field] = (acc[field] || 0) + 1;
+  return acc;
+}, {});
+console.log(countDepartments);
