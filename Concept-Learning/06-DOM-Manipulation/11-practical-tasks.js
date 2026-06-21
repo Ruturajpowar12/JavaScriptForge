e24e33259bf43b49dd9c067ae0dc6818926955ba
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-06-21 13:20:20 +0530

    add: implement bank account, cricket scoreboard, online order system, and user login programs

--- Concept-Learning/06-DOM-Manipulation/11-practical-tasks.js
+++ Concept-Learning/06-DOM-Manipulation/11-practical-tasks.js
@@ -74,3 +74,68 @@ function shoppingBill() {
   console.log("Total               ", total);
 }
 shoppingBill();
+
+// Topic 2: Variables & Constants (var, let, const)
+
+// Task 1
+// Create a bank account program.
+// Store account holder name and balance.
+// Update the balance after a deposit.
+
+function BankAccount() {
+  const accountHolderName = "Ruturaj";
+  let balance = 5000;
+  let deposit = 2000 + balance;
+  console.log("balance: ", balance);
+  console.log("after Deposit balance: ", deposit);
+}
+BankAccount();
+
+// Task 2
+// Create a cricket scoreboard.
+// Store team name using const.
+// Store current score using let.
+// Update the score multiple times.
+function scoreBoard() {
+  const teamName = "india";
+  let currentScore = 156;
+  const six = 6;
+  const four = 4;
+  currentScore = currentScore + six;
+  currentScore = currentScore + four;
+  currentScore = currentScore + 3;
+  currentScore = currentScore + six;
+  console.log(`Team ${teamName} score is ${currentScore}`);
+}
+scoreBoard();
+// Task 3
+// Create an online order system.
+// Store order id using const.
+// Store order status using let.
+// Change the order status 3 times.
+function onlineOrderSystem() {
+  const orderId = 174;
+  let isComplete = "pending";
+  isComplete = "delivered";
+  isComplete = "canceled";
+  console.log(isComplete);
+}
+onlineOrderSystem();
+// Task 4
+// Create a user login system.
+// Store username and login count.
+// Increase login count after every login.
+function userLogin() {
+  const username = "CodeNinja99";
+  let loginCount = 0;
+  console.log(`User: ${username} | Initial Logins: ${loginCount}`);
+  loginCount++;
+  console.log(`Login successful! Current count: ${loginCount}`);
+
+  loginCount++;
+  console.log(`Login successful! Current count: ${loginCount}`);
+
+  loginCount++;
+  console.log(`Login successful! Current count: ${loginCount}`);
+}
+userLogin();
